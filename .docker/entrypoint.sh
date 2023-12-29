533e3c068357bdc5dbc16e549baf33c8972a47b6
Jan Mikeš <j.mikes@me.com>
2023-12-29 12:37:13 +0100

    Rework to use docker

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+fi
+
+composer install
+
+mkdir -p www/webtemp
+chmod 777 www/webtemp
+
+mkdir -p temp/sessions
+chmod 777 temp/sessions
+
+mkdir -p log
+chmod 777 log
+
+mkdir -p www/upload/
+chmod 777 www/upload/
+
+mkdir -p www/img/gallery/
+chmod 777 www/img/gallery/
+
+exec "$@"
